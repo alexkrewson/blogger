b019aa47c4ee79dfdc2ab79c39ed8cd8381df36e
alexkrewson <alex.krewson@gmail.com>
2019-04-08 15:48:34 -0700

    added comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
   Blogger::Application.routes.draw do
-    resources :articles
+
+
+    resources :articles do
+      resources :comments
+    end
+    
   end
   
 
